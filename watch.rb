489ddce23b87472cb1298be09f5f3c4bd0eacd87
TADA Tadashi <t@tdtds.jp>
2011-11-23 22:35:07 +0900

    caught error in tweet.

--- watch.rb
+++ watch.rb
@@ -20,23 +20,29 @@ end
 
 search_string = '#gracoro_union'
 
-TweetStream::Client::new.on_error {|message|
-	puts message
-}.track( search_string ) do |status|
-	begin
-		next if status.text.index '@' # skipping replies included.
-		person = Person.where( name: status.user[:screen_name] ).first ||
-			Person::create( name: status.user[:screen_name], count: 0 )
-		person.count += 1
-		person.save
-		Tweet::create(
-			_id: status.id,
-			name: status.user[:screen_name],
-			text: status.text ).save
-		p status.user[:screen_name]
-	rescue
-		p "ERROR: #$!"
+begin
+	TweetStream::Client::new.on_error {|message|
+		puts message
+	}.track( search_string ) do |status|
+		begin
+			next if status.text.index '@' # skipping replies included.
+			person = Person.where( name: status.user[:screen_name] ).first ||
+				Person::create( name: status.user[:screen_name], count: 0 )
+			person.count += 1
+			person.save
+			Tweet::create(
+				_id: status.id,
+				name: status.user[:screen_name],
+				text: status.text ).save
+			p status.user[:screen_name]
+		rescue
+			p "ERROR: #$!"
+		end
 	end
+rescue
+	p "ERROR: #$!"
+	sleep 10
+	retry
 end
 
 # [:truncated, :text, :id_str, :retweeted, :in_reply_to_status_id_str, :in_reply_to_user_id, :in_reply_to_user_id_str, :entities, :source, :created_at, :contributors, :place, :geo, :favorited, :coordinates, :user, :in_reply_to_status_id, :id, :retweet_count, :in_reply_to_screen_name]
