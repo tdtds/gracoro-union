b37bd43731c9424425242201f1d65064342a5bd5
TADA Tadashi <t@tdtds.jp>
2011-11-25 19:25:22 +0900

    show people and count total.

--- public/js/top.js
+++ public/js/top.js
@@ -2,6 +2,13 @@
  * top.js: client side script of top page on gracoro union.
  */
 $(function(){
+	var people = 0, count = 0;
+	$('#ranking li span').each(function(){
+		people++;
+		count += parseInt($(this).text());
+	});
+	$('#ranking h2').append('&nbsp;<span class="count">' + people + '人で食べたグラコロ、ただいま' + count + '</span>');
+
 	if($('#ranking li.hide').size() > 0){
 		$('#ranking').append('<a class="showall" href="">すべて表示</a>');
 		$('#ranking a.showall').click(function(){
